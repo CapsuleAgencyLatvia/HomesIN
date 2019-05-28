6fcb14334523c2ed543356814c06cd20e814780e
unknown <elvis.rauza@gmail.com>
2019-05-28 15:57:41 +0300

    Added loop for header icons

--- dashboards/modules/header.php
+++ dashboards/modules/header.php
@@ -34,21 +34,38 @@
                         <input class="input" type="text" name="" value="" placeholder="Search">
                     </form>
                 </div>
-                <div class="col m-left">
-                    <button class="icon-wrapper user-add">
-                        <img class="icon lazy" data-src="/global/assets/img/user-add.svg" alt="Users">
-                    </button>
-                </div>
-                <div class="col">
-                    <button class="icon-wrapper notifications">
-                        <img class="icon lazy" data-src="/global/assets/img/notifications.svg" alt="Notifications">
-                    </button>
-                </div>
-                <div class="col">
-                    <button class="icon-wrapper question">
-                        <img class="icon lazy" data-src="/global/assets/img/question-mark.svg" alt="Questions">
-                    </button>
-                </div>
+                <?php
+                $blocks = array(
+                    array(
+                        'icon' => array(
+                            'url' => '/global/assets/img/user-add.svg',
+                            'alt' => 'Users',
+                        ),
+                    ),
+                    array(
+                        'icon' => array(
+                            'url' => '/global/assets/img/notifications.svg',
+                            'alt' => 'Notifications',
+                        ),
+                    ),
+                    array(
+                        'icon' => array(
+                            'url' => '/global/assets/img/question-mark.svg',
+                            'alt' => 'Questions',
+                        ),
+                    ),
+                );
+                $cnt = 0;
+                foreach ( $blocks as $block ) :
+                    $cnt == 0 ? $class = 'm-left' : $class = ''; ?>
+                    <div class="col <?php echo $class; ?>">
+                        <button class="icon-wrapper">
+                            <img class="icon lazy" data-src="<?php echo $block['icon']['url']; ?>" alt="<?php echo $block['icon']['alt']; ?>">
+                        </button>
+                    </div>
+                    <?php
+                    $cnt++;
+                endforeach; ?>
                 <div class="col">
                     <div class="user-wrapper">
                         <button class="icon-wrapper user">
