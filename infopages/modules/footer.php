6b9b68083d173b84eb89ef30723be5f530f2c158
unknown <elvis.rauza@gmail.com>
2019-05-28 11:54:23 +0300

    Adding support for global files

--- infopages/modules/footer.php
+++ infopages/modules/footer.php
@@ -33,8 +33,8 @@
     </div>
 </footer>
 
-<script src="assets/js/lazyload.min.js" charset="utf-8"></script>
-<script src="assets/js/jquery.min.js" charset="utf-8"></script>
+<script src="/global/assets/js/lazyload.min.js" charset="utf-8"></script>
+<script src="/global/assets/js/jquery.min.js" charset="utf-8"></script>
 <script src="assets/js/script.js" charset="utf-8"></script>
 </body>
 </html>
