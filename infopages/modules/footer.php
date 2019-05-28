833c8bd1f7999e40c85b7f647746d61e2587f97f
unknown <elvis.rauza@gmail.com>
2019-05-28 11:22:19 +0300

    Removed Owl style and script, not jet required

--- infopages/modules/footer.php
+++ infopages/modules/footer.php
@@ -35,7 +35,6 @@
 
 <script src="assets/js/lazyload.min.js" charset="utf-8"></script>
 <script src="assets/js/jquery.min.js" charset="utf-8"></script>
-<script src="assets/js/owl.carousel.min.js" charset="utf-8"></script>
 <script src="assets/js/script.js" charset="utf-8"></script>
 </body>
 </html>
