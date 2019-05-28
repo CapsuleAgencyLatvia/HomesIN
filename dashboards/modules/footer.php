15fecf575ccb0a8bf3e42a756f1db7068c42af1c
unknown <elvis.rauza@gmail.com>
2019-05-28 11:22:50 +0300

    Registered provider dashboard base

---
+++ dashboards/modules/footer.php
@@ -0,0 +1,5 @@
+<script src="assets/js/lazyload.min.js" charset="utf-8"></script>
+<script src="assets/js/jquery.min.js" charset="utf-8"></script>
+<script src="assets/js/script.js" charset="utf-8"></script>
+</body>
+</html>
